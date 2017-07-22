cdec98df70235df356963605fdee5dcf4544f376
Yannick Chiasson <yan@shiyason.com>
2017-07-22 18:09:26 +0900

    make miguSongStore static property

--- ZhongWenKaraoke/AppDelegate.swift
+++ ZhongWenKaraoke/AppDelegate.swift
@@ -12,10 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    static let miguSongStore = MiguSongStore()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        var miguSongStore = MiguSongStore()
         // Override point for customization after application launch.
         return true
     }
