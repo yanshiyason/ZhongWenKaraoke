d137c16334af7a3a8b3f8c998d9502231ec32c50
Yannick Chiasson <yan@shiyason.com>
2017-07-25 06:56:40 +0900

    create jukebox outlets

--- ZhongWenKaraoke/Controllers/JukeboxViewController.swift
+++ ZhongWenKaraoke/Controllers/JukeboxViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class JukeboxViewController: UIViewController {
 
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var prevBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
+    @IBOutlet weak var restartBtn: UIButton!
+    @IBOutlet weak var songProgress: UIProgressView!
+    
+//    var jukebox: Jukebox!
     override func viewDidLoad() {
         super.viewDidLoad()
 
